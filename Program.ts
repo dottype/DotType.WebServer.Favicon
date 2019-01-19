ab84b19f32ac2aab63bde0f9f26a5215a71cc109
Ross Rotaru <rostislav@rotaru.me>
2019-01-18 21:07:13 -0800

    Removed console.log()

--- Program.ts
+++ Program.ts
@@ -1,7 +1,6 @@
 import { WebServer } from "dottype.webserver/WebServer";
 import { FaviconMiddleware } from "./FaviconMiddleware";
 
-console.log("HERE");
 new WebServer()
     .UseMiddleware(new FaviconMiddleware())
     .RunAsync();
